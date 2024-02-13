5bf90a53b47500c892d8e3bf9bd69caa318f78fd
cfeliciano <cfeliciano7792@gmail.com>
2024-02-13 12:55:55 -0500

    Added microservice folder

---
+++ microservice/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+// List of D&D classes
+const dndClasses = [
+  'Barbarian',
+  'Bard',
+  'Cleric',
+  'Druid',
+  'Fighter',
+  'Monk',
+  'Paladin',
+  'Ranger',
+  'Rogue',
+  'Sorcerer',
+  'Warlock',
+  'Wizard'
+];
+
+// Route to get a random D&D class
+app.get('/random-dnd-class', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * dndClasses.length);
+  const randomClass = dndClasses[randomIndex];
+  res.json({ class: randomClass });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
