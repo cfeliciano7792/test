02c16bc1fb0e53626354319486c6a4e4b3bd90c3
cfeliciano <cfeliciano7792@gmail.com>
2024-02-19 09:25:40 -0500

    changed fetch request

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [randomDndClass, setRandomDndClass] = useState("");
 
   const getRandomDndClass = () => {
-  fetch('http://localhost:3000/random-dnd-class')
+    // http://localhost:3000/random-dnd-class
+  fetch('https://dnd-generator.onrender.com/random-class')
       .then(response => response.json())
       .then(data => setRandomDndClass(data.class))
       .catch(error => console.error('Error fetching random D&D class:', error));
